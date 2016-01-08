sonyxperiadev/device-sony-shinano

Add a simple PowerHAL for native double-tap-to-wake
We dropped the QCOM PowerHAL because it relies on too many
proprietary blobs. Instead we have a kernel solution, but
several features still depend on the presence of a PowerHAL,
so Android is actually not happy about it.

Marshmallow introduced native double-tap-to-wake support,
which is built into the PowerHAL. This commit enables DTTW
and adds a very simple PowerHAL to support this feature.

Hopefully we can later extend this when our kernel driver
can accept changes from userspace.

Signed-off-by: Adam Farden <adam@farden.cz>

device.mk
@@ -200,6 +200,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libboringssl-compat
 
+# Simple PowerHAL
+PRODUCT_PACKAGES += \
+    power.shinano
+
 # APN list
 PRODUCT_COPY_FILES += \
     device/sample/etc/old-apns-conf.xml:system/etc/old-apns-conf.xml \

power/Android.mk
@@ -1,0 +1,34 @@
+# Copyright (C) 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_SHARED_LIBRARIES := liblog libcutils
+LOCAL_SRC_FILES := power.c
+
+ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
+    LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
+
+ifeq ($(TARGET_TAP_TO_WAKE_STRING),true)
+    LOCAL_CFLAGS += -DTAP_TO_WAKE_STRING
+endif
+endif
+
+LOCAL_MODULE := power.shinano
+
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)

CyanogenMod/android_device_sony_yukon

Change-Id: I25b2ca59685f48e0d8d15f32d2b3fd76ea8920d5

device.mk
@@ -184,6 +184,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.sh
 
+# Simple PowerHAL
+PRODUCT_PACKAGES += \
+    power.yukon
+
 # APN list
 PRODUCT_COPY_FILES += \
     device/sample/etc/old-apns-conf.xml:system/etc/old-apns-conf.xml \

power/Android.mk
@@ -1,0 +1,34 @@
+# Copyright (C) 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_SHARED_LIBRARIES := liblog libcutils
+LOCAL_SRC_FILES := power.c
+
+ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
+    LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
+
+ifeq ($(TARGET_TAP_TO_WAKE_STRING),true)
+    LOCAL_CFLAGS += -DTAP_TO_WAKE_STRING
+endif
+endif
+
+LOCAL_MODULE := power.yukon
+
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)