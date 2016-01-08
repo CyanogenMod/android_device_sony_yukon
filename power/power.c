08aa2ec8a303f17ca509617f4de618fe7b1b259f
Adam Farden <adam@farden.cz>
2016-01-08 03:22:12 +0100

    power: upgrade for CM13
    Change-Id: If5701afd0a90ca2ee813f3ce2b5e9ded839bfdd5
    Signed-off-by: Adam Farden <adam@farden.cz>

--- power/power.c
+++ power/power.c
@@ -36,7 +36,6 @@ static int is_8928 = -1;
 static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 static int boostpulse_fd = -1;
 
-static int is_low_power_mode = 0;
 static int current_power_profile = -1;
 static int requested_power_profile = -1;
 
@@ -149,13 +148,6 @@ static void set_power_profile(int profile)
         return;
     }
 
-    if (is_low_power_mode) {
-        /* Let's assume we get a valid profile */
-        requested_power_profile = profile;
-        ALOGD("%s: low power mode enabled, ignoring profile change request", __func__);
-        return;
-    }
-
     if (profile == current_power_profile)
         return;
 
@@ -179,23 +171,6 @@ static void set_power_profile(int profile)
                     profiles[profile].scaling_max_freq_28 : profiles[profile].scaling_max_freq_26);
 
     current_power_profile = profile;
-}
-
-static void set_low_power_mode(int on)
-{
-    if (on == is_low_power_mode)
-        return;
-
-    ALOGD("%s: state=%d", __func__, on);
-
-    if (on) {
-        requested_power_profile = current_power_profile;
-        set_power_profile(PROFILE_POWER_SAVE);
-        is_low_power_mode = 1;
-    } else {
-        is_low_power_mode = 0;
-        set_power_profile(requested_power_profile);
-    }
 }
 
 static void power_hint(__attribute__((unused)) struct power_module *module,
@@ -228,10 +203,13 @@ static void power_hint(__attribute__((unused)) struct power_module *module,
         }
         }
         break;
-    case POWER_HINT_LOW_POWER:
+    case POWER_HINT_SET_PROFILE:
         pthread_mutex_lock(&lock);
-        set_low_power_mode(*(int32_t *)data ? 1 : 0);
+        set_power_profile(*(int32_t *)data);
         pthread_mutex_unlock(&lock);
+        break;
+    case POWER_HINT_LOW_POWER:
+        /* This hint is handled by the framework */
         break;
     default:
         break;
@@ -241,6 +219,15 @@ static void power_hint(__attribute__((unused)) struct power_module *module,
 static struct hw_module_methods_t power_module_methods = {
     .open = NULL,
 };
+
+static int get_feature(__attribute__((unused)) struct power_module *module,
+                       feature_t feature)
+{
+    if (feature == POWER_FEATURE_SUPPORTED_PROFILES) {
+        return PROFILE_MAX;
+    }
+    return -1;
+}
 
 struct power_module HAL_MODULE_INFO_SYM = {
     .common = {
@@ -256,4 +243,5 @@ struct power_module HAL_MODULE_INFO_SYM = {
     .init = power_init,
     .setInteractive = power_set_interactive,
     .powerHint = power_hint,
+    .getFeature = get_feature
 };
