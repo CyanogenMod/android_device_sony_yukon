15c899e8bace0f762c4bf4caed5a050d33902120
Adam Farden <adam@farden.cz>
2016-01-10 19:42:20 +0100

    init.sh: remove keyckecks, only check for warmboot
    We now have a way to enter recovery from power off, removing the
    keychecks will save us the 3 seconds delay.
    
    Change-Id: Ifea3d6c554781559c74f632c04f06ad68a666298
    Signed-off-by: Adam Farden <adam@farden.cz>

--- init/init.sh
+++ init/init.sh
@@ -24,50 +24,13 @@ busybox mknod -m 666 /dev/null c 1 3
 # include device specific vars
 source /sbin/bootrec-device
 
-led_amber() {
-  busybox echo 255 > ${BOOTREC_LED_RED}
-  busybox echo 255 > ${BOOTREC_LED_GREEN}
-  busybox echo   0 > ${BOOTREC_LED_BLUE}
-}
-
-led_orange() {
-  busybox echo 255 > ${BOOTREC_LED_RED}
-  busybox echo 100 > ${BOOTREC_LED_GREEN}
-  busybox echo   0 > ${BOOTREC_LED_BLUE}
-}
-
-led_off() {
-  busybox echo   0 > ${BOOTREC_LED_RED}
-  busybox echo   0 > ${BOOTREC_LED_GREEN}
-  busybox echo   0 > ${BOOTREC_LED_BLUE}
-}
-
-busybox mkdir -m 755 /dev/input
-busybox mknod -m 600 ${BOOTREC_EVENT_NODE}
-
-KEYLOG=key-events.txt
-busybox cat ${BOOTREC_EVENT} > ${KEYLOG} &
-
-led_amber
-
-busybox sleep 3
-busybox pkill -f "cat ${BOOTREC_EVENT}"
-
-# Yes, we really want to enter recovery
-ywrwter=`busybox wc -c <${KEYLOG}`
-echo "key event size is: $ywrwter" >> ${LOG}
-
-# Check for key events, or being explicity asked to go into recovery mode
+# Check the cmdline if the OS asked us to boot into recovery mode
 # See kernel/arch/arm/mach-msm/restart.c (msm_restart_prepare function) for
 # a mapping of reboot modes to constants.
 warmboot_recovery=0x77665502
 
-# Why 32? A kernel bug means that the key event is not null, so
-# we check for a size greater than the buggy value. A real key
-# event will have a size in the hundreds.
-if [ $ywrwter -gt 32 ] || busybox grep -q warmboot=${warmboot_recovery} /proc/cmdline; then
+if busybox grep -q warmboot=${warmboot_recovery} /proc/cmdline; then
   echo "Entering Recovery mode" >> ${LOG}
-  led_orange
   busybox mkdir -m 755 -p /dev/block
   busybox mknod -m 600 ${BOOTREC_FOTA_NODE}
   busybox mount -o remount,rw /
@@ -81,7 +44,6 @@ if [ $ywrwter -gt 32 ] || busybox grep -q warmboot=${warmboot_recovery} /proc/cm
 else
   echo "Booting Normally" >> ${LOG}
 fi
-led_off
 
 echo "PATH=${PATH}" >> ${LOG}
 echo "_PATH=${_PATH}" >> ${LOG}
