b76f4b9106cd78d8922013f39c30a57e6c8dcd02
Adam Farden <adam@farden.cz>
2016-01-12 20:16:27 +0100

    init: use releasetools to set the firmware symlinks
    All devices have a variant specific LTALabel partition. Mount it
    and find the variant from the html file on this partition.
    
    Thanks @invisiblek for showing me this exists
    https://github.com/CyanogenMod/android_device_htc_m8-common/tree/cm-13.0/releasetools
    
    Change-Id: I5c6db61db74bad79d27a16a0ee32e234bb733d08

---
+++ releasetools/firmware.sh
@@ -0,0 +1,28 @@
+#!/sbin/sh
+
+set -e
+
+# Get the yukon variant
+deviceid=`getprop ro.cm.device`
+
+# Detect the exact model from the LTALabel partition
+# This looks something like:
+# 1284-8432_5-elabel-D5303-row.html
+mkdir -p /lta-label
+mount -r -t ext4 /dev/block/platform/msm_sdcc.1/by-name/LTALabel /lta-label
+variant=`ls /lta-label/*.html | sed s/.*-elabel-// | sed s/-row.html// | tr -d '\n\r'`
+umount /lta-label
+
+# Set the variant as a prop
+touch /system/vendor/build.prop
+echo ro.fxp.variant=$variant >> /system/vendor/build.prop
+
+if [ $deviceid == "tianchi" ] || [ $deviceid == "flamingo" ]; then
+    # Symlink the correct modem blobs
+    basedir="/system/blobs/$variant/"
+    cd $basedir
+    find . -type f | while read file; do ln -s $basedir$file /system/etc/firmware/$file ; done
+fi;
+
+exit 0
+
