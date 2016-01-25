3f591f05a775f4b4856ceb7f11cbf2e473ad692c
Vladimir <v.seregin.its@gmail.com>
2016-01-25 16:54:31 +0100

    Add hostapd config
    Fixes WiFi tethering
    
    Change-Id: I98942d7c13337c13d0d9fa9e1621f08e2592bd5a

--- device.mk
+++ device.mk
@@ -26,6 +26,7 @@ PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/init.yukon.usb.rc:root/init.yukon.usb.rc \
     $(SONY_ROOT)/init.yukon.pwr.rc:root/init.yukon.pwr.rc \
     $(SONY_ROOT)/system/usr/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(SONY_ROOT)/rootdir/system/etc/hostapd/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
     $(SONY_ROOT)/system/etc/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     $(SONY_ROOT)/system/etc/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(SONY_ROOT)/system/etc/init.yukon.bt.sh:system/etc/init.yukon.bt.sh \
